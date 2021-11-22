c2dc8e1569c6ca2155e818255fc4ffd4dbdaa1b0
Paul Fouquet <pfouquet@linz.govt.nz>
2021-11-22 13:17:21 +1300

    example: add a conditional jsonschema

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ Ajv = require('ajv');
 
 async function test(){
     let schemaDual = require('./data/schemas/dual.json');
+    let schemaDualCond = require('./data/schemas/dual-conditional.json');
     let schemaSingle = require('./data/schemas/single.json');
     let testFile = require('./data/to_validate/test.json');
     const ajv = new Ajv();
 
     validatorDual = ajv.compile(schemaDual);
+    validatorDualCond = ajv.compile(schemaDualCond);
     validatorSingle = ajv.compile(schemaSingle);
 
     validatorDual(testFile);
+    validatorDualCond(testFile);
     validatorSingle(testFile);
 
     validatorSingle.errors.forEach(error => {
@@ -18,6 +21,9 @@ async function test(){
     });
     validatorDual.errors.forEach(error => {
         console.log("Error for dual schema: ", error.instancePath, " ", error.message)
+    });
+    validatorDualCond.errors.forEach(error => {
+        console.log("Error for dual conditional schema: ", error.instancePath, " ", error.message)
     });
 }
 
--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import os
 import jsonschema_rs
 
 schema_dual = open(os.path.join(os.getcwd(), "data", "schemas", "dual.json"), "r")
+schema_dual_conditional = open(os.path.join(os.getcwd(), "data", "schemas", "dual-conditional.json"), "r")
 schema_single = open(os.path.join(os.getcwd(), "data", "schemas", "single.json"), "r")
 test_file = open(os.path.join(os.getcwd(), "data", "to_validate", "test.json"), "r")
 test_dict = json.loads(test_file.read())
 validator_dual = jsonschema_rs.JSONSchema.from_str(schema_dual.read())
+validator_dual_conditional = jsonschema_rs.JSONSchema.from_str(schema_dual_conditional.read())
 validator_single = jsonschema_rs.JSONSchema.from_str(schema_single.read())
 
 errors_single = validator_single.iter_errors(test_dict)
@@ -17,3 +19,7 @@ for error in errors_single:
 errors_dual = validator_dual.iter_errors(test_dict)
 for error in errors_dual:
     print("Error for dual schema: ", error.message)
+
+errors_dual_cond = validator_dual_conditional.iter_errors(test_dict)
+for error in errors_dual_cond:
+    print("Error for dual conditional schema: ", error.message)
